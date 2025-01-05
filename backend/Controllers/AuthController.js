e9bbf5f3e09a377b214fc9d993ec6c0800590251
ahmadammad908 <ahmadammad789@gmail.com>
2025-01-05 17:09:21 +0500

    first commit

--- backend/Controllers/AuthController.js
+++ backend/Controllers/AuthController.js
@@ -1,6 +1,6 @@
 const UserModel = require("../Models/User");
 const bcrypt = require('bcrypt')
-// const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const signup = async (req,res)=> {
     try {
         const {name,email,password} =req.body;
@@ -30,6 +30,47 @@ const signup = async (req,res)=> {
 
 
 
+
+
+const login = async (req,res)=> {
+    try {
+        const {email,password} =req.body;
+        const user = await UserModel.findOne({email});
+        const errorMessage = "Auth failed email or password is wrong";
+        if(!user){
+            return res.status(403)
+            .json({message:errorMessage, success:false})
+        }
+       const isPassEqual = await bcrypt.compare(password,user.password);
+       if(!isPassEqual){
+        return res.status(403)
+        .json({message:errorMessage, success:false});
+       }
+       const jwtToken = jwt.sign({email: user.email, _id:user._id},
+        process.env.JWT_SECRET,
+        {expiresIn: '24h'}
+       )
+        res.status(200)
+        .json({
+            message:"Login successfully",
+            success:true,
+            jwtToken,
+            email,
+            name:user.name
+        })
+    } catch (error) {
+        res.status(500)
+        .json({
+            message:"Internal Server Error",
+            success:false
+        })
+    }
+}
+
+
+
+
 module.exports = {
-    signup
+    signup,
+    login
 }
