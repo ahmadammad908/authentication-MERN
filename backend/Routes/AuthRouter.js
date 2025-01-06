505f16667fd50fa40fec8a2b61364e9ab31c6d70
ahmadammad908 <ahmadammad789@gmail.com>
2025-01-06 15:05:24 +0500

    first commit

--- backend/Routes/AuthRouter.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const { signup,login } = require('../Controllers/AuthController');
-const {signupValidation, loginValidation} = require('../Middlewares/AuthVaildation')
-
-router.post('/login', loginValidation,login); 
-router.post('/signup', signupValidation,signup); 
-module.exports = router
